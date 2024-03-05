19d42e49e7276a81c24bd74f8f24a4d0e9139a8e
QuentinLEGROS <legros.quentin2@hotmail.fr>
2024-03-05 16:21:36 +0100

    Delete Compute_Amplitude_DF directory

--- Compute_Amplitude_DF/reduced_time_axis.m
+++
@@ -1,9 +0,0 @@
-function rt=reduced_time_axis(N)
-  if isodd(N)
-    H=(N-1)/2;
-    rt=[-H:+H];
-  else
-    H=N/2-1;
-    rt=[-H-1:H];
-  end
-end
--- Compute_Amplitude_DF/time_axis.m
+++
@@ -1,3 +0,0 @@
-function t=time_axis(N,Fs)
-  t=reduced_time_axis(N)/Fs;
-end
